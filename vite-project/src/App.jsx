1e957bf3d8ec0ad5cd16876cbc3856a9e5b1ba5d
Aiden Cullo <aidencullo@gmail.com>
2024-09-25 11:59:09 -0400

    add actual text given

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,20 +1,20 @@
 import React, { useState } from 'react'
 
-import AddTodo from './components/AddTodo'
+import Todo from './components/AddTodo'
 import TodoList from './components/TodoList'
 
 function App() {
   const [todos, setTodos] = useState([])
 
-  const handleClick = () => {
-    setTodos([...todos, 'New Todo'])
+  const handleClick = (todo) => {
+    setTodos([...todos, todo])
   }
   
   return (
     <>
       <h1>Create Todo List</h1>
-      <AddTodo
-        handleClick={handleClick}
+      <Todo
+       addTodo={handleClick}
       />
       <TodoList
 	todos={todos}
--- vite-project/src/components/AddTodo.jsx
+++ vite-project/src/components/AddTodo.jsx
@@ -1,7 +1,21 @@
-export default function AddTodo({ handleClick }) {
+import React, { useState } from "react";
+
+export default function Todo({ addTodo }) {
+  const [text, setText] = useState("");
+
+  const handleClick = () => {
+    addTodo(text);
+    setText("");
+  };
+  
   return (
     <div>
-      <input type="text" placeholder="Enter your todo" />
+      <input
+        type="text"
+        placeholder="Enter your todo"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
       <button onClick={handleClick}>Add Todo</button>
     </div>
   );
