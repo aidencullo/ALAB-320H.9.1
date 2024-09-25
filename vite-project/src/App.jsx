ffb60764ae85aa410392ef3a66fe89412eda7879
Aiden Cullo <aidencullo@gmail.com>
2024-09-25 11:49:56 -0400

    add todos

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,23 @@
+import React, { useState } from 'react'
+
 function App() {
+  const [todos, setTodos] = useState([])
+
+  const handleClick = () => {
+    setTodos([...todos, 'New Todo'])
+  }
+  
   return (
     <>
       <h1>Create Todo List</h1>
+      <input type="text" placeholder="Enter your todo" />
+      <button onClick={handleClick}>Add Todo</button>
+
+      <ul>
+	{todos.map((todo, index) => (
+	  <li key={index}>{todo}</li>
+	))}
+      </ul>
     </>
   )
 }
